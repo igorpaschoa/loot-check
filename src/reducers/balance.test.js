f77e73f5eef32f7d20b5e9b6a6219ee98b241dfa
Igor Paschoa <igor.paschoa@vistajet.com>
2018-11-28 10:57:37 -0200

    added logic to deposit/withdraw from wallet

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -9,4 +9,22 @@ describe('balanceReducer', () => {
       balanceReducer(undefined, { type: constants.SET_BALANCE, balance })
     ).toEqual(balance);
   });
+
+  it('deposits into the balance', () => {
+    const deposit = 10;
+    const initialState = 5;
+
+    expect(
+      balanceReducer(initialState, { type: constants.DEPOSIT, deposit })
+    ).toEqual(initialState + deposit);
+  });
+
+  it('withdraws from the balance', () => {
+    const withdraw = 10;
+    const initialState = 25;
+
+    expect(
+      balanceReducer(initialState, { type: constants.WITHDRAW, withdraw })
+    ).toEqual(initialState - withdraw);
+  });
 });
